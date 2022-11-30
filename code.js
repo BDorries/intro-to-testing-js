e9fc2314bc5d08c0b529750751a774106379be6d
Billie <billie.dorries28@gmail.com>
2022-11-30 16:56:10 -0600

    made most of the testing cases

--- code.js
+++ code.js
@@ -2,3 +2,25 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(str){
+    if (typeof str !== "string"){
+        return "Hello, World!"
+    }
+    return `Hello, ${str}`;
+}
+
+function isFive(num){
+    if(typeof num !== 'number')
+    return false;
+    return parseInt(num) === 5;
+}
+
+function isEven(input){
+    return parseFloat(input) % 2 === 0;
+}
+
+function isVowel(char){
+    let vowels = ['a','e','i','o','u'];
+    return vowels.includes(char.toLowerCase());
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,76 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane" when Jane is passed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane");
+    });
+    it('should return the string "Hello, Alex" when Alex is passed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex");
+    });
+    it('should return the string "Hello, Pat" when Pat is passed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat");
+    });
+    it('should return the string "Hello, World!" when true is passed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when false is passed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+});
+
+describe('isFive', function() {
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
+    });
+    it('should return false if 4 is passed', function() {
+        expect(isFive(4)).toBe(false);
+    });
+    it("should return false if '5' is passed", function() {
+        expect(isFive('5')).toBe(false);
+    });
+    it("should return false if 'Hello' is passed", function() {
+        expect(isFive("Hello")).toBe(false);
+    });
+});
+
+describe('isEven', function() {
+    it('should be a defined function', () => {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return true when passed 2', () => {
+        expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed -4', () => {
+        expect(isEven(-4)).toBe(true);
+    });
+    it('should return true when passed "8"', () => {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return true when passed 0', () => {
+        expect(isEven(0)).toBe(true);
+    });
+    it('should return false when passed the number 3', () => {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return false when passed a non-numeric string like "banana"', () => {
+        expect(isEven("banana")).toBe(false);
+    });
+
+});
+
+describe('isVowel', function(){
+   it(`should be a defined function`, () => {
+       expect(typeof isVowel).toBe('function');
+   });
+   it("should return true if 'a' is passed", () => {
+       expect(isVowel('a')).toBe(true);
+   })
+});
